8c636e69fb965df1a45483263c132c2578115398
user <user@localhost>
2020-01-01 12:52:40 +0000

    00000039-8428aa19

---
+++ precache-manifest.0e72abda7bba097c063c9558302edf38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed8d0e66eb6120deb27a1eb7c6556376",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8dbce851a8243386fdc",
+    "url": "/static/js/3.49092286.chunk.js"
+  },
+  {
+    "revision": "eab6a173e87a331f09476204845ab06a",
+    "url": "/static/js/3.49092286.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20d92508c0db0b63f880",
+    "url": "/static/js/arena.json.ccebc79d.chunk.js"
+  },
+  {
+    "revision": "6a0fd28fd607e697d06c",
+    "url": "/static/js/main.602862e0.chunk.js"
+  },
+  {
+    "revision": "494dcf7289bf54d83f75",
+    "url": "/static/js/runtime-main.ded7aa8a.js"
+  }
+]);
